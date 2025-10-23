a4eca4bfcf9b4cc76a84d8f29468142660cc88c5
dlssite <sanctyr.dls@gmail.com>
2025-10-23 10:12:50 -0700

    Cors Fixed

--- handlers/server.js
+++ handlers/server.js
@@ -5,6 +5,7 @@ const app = express();
 const port = 3001;
 app.get('/', (req, res) => {
     const imagePath = path.join(__dirname, 'index.html');
+    res.setHeader('Content-Security-Policy', "default-src 'self'; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src https://fonts.googleapis.com https://fonts.gstatic.com; img-src 'self' https://emberlyn.onrender.com");
     res.sendFile(imagePath);
 });
 app.listen(port, () => {
